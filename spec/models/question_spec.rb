58054d8e0fbbb697f2953852a207a637d1b418ac
jiop <jeremie.iop@gmail.com>
2016-09-29 00:18:46 +0200

    Add some tests for Question model

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { described_class.new }
+
+  context 'model validations and associations' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+    it { should belong_to :user }
+  end
 end
